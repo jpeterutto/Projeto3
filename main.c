c5818e3215cf1a7cded7a107de9b53cd606c0582
jpeterutto <149285505+jpeterutto@users.noreply.github.com>
2023-11-11 18:59:19 -0300

    Alterar Tarefas adicionado

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include "projeto.h"
 
+// João Pedro Lopes - RA: 24.123.071-3
+// João Pedro Peterutto - RA: 24.123.045-7
+// Gabriel Koiama - RA: 24.123.051-5
+
 int main() {
     struct Tarefa t[100];
     FILE *arquivo_binario = fopen("tarefas.txt", "rb");
@@ -21,7 +25,8 @@ int main() {
         printf(" 1 - Criar\n");
         printf(" 2 - Excluir\n");
         printf(" 3 - Listar\n");
-        printf(" 4 - Sair\n");
+        printf(" 4 - Alterar\n");
+        printf(" 5 - Sair\n");
         printf("Digite o numero da opcao que deseja usar: ");
 
         char *p, s_opcao[100];
@@ -30,56 +35,50 @@ int main() {
         fgets(s_opcao, sizeof(s_opcao), stdin);
         opcao = strtol(s_opcao, &p, 10);
 
-        if (opcao == 1) {
-            ler(&t[cont]);
-            printf("Tarefa criada!\n\n");
-            cont++;
-        } else if (opcao == 2) {
-            printf("Digite o numero da tarefa que deseja excluir: ");
-            fgets(s_posicao, sizeof(s_posicao), stdin);
-            posicao = strtol(s_posicao, &p_posicao, 10);
+        switch (opcao) {
+            case 1:
+                ler(&t[cont]);
+                printf("Tarefa criada!\n\n");
+                cont++;
+                break;
+            case 2:
+                printf("Digite o numero da tarefa que deseja excluir: ");
+                fgets(s_posicao, sizeof(s_posicao), stdin);
+                posicao = strtol(s_posicao, &p_posicao, 10);
 
-            if (p_posicao == s_posicao || *p_posicao != '\n') {
-                printf("Valor invalido!\n\n");
-            } else {
-                excluir_tarefa(t, &cont, posicao);
-            }
-        } else if (opcao == 3) {
-            printf("Lista:\n\n");
-            for (int x = 0; x < cont; x++) {
-                printf("Tarefa %d\n", x + 1);
-                printf("Prioridade: %d\n", t[x].prioridade);
-                printf("Categoria: %s\n", t[x].categoria);
-                printf("Descricao: %s\n", t[x].descricao);
-
-                // Mostrar o status da tarefa
-                printf("Status: ");
-                switch (t[x].status) {
-                    case NAO_INICIADO:
-                        printf("Não Iniciado\n");
-                        break;
-                    case EM_ANDAMENTO:
-                        printf("Em Andamento\n");
-                        break;
-                    case COMPLETO:
-                        printf("Completo\n");
-                        break;
-                    default:
-                        printf("Desconhecido\n");
+                if (p_posicao == s_posicao || *p_posicao != '\n') {
+                    printf("Valor inválido!\n\n");
+                } else {
+                    excluir_tarefa(t, &cont, posicao);
                 }
-
-                printf("\n");
-            }
-        } else if (opcao == 4) {
-            arquivo_binario = fopen("tarefas.txt", "wb");
-            if (arquivo_binario) {
-                fwrite(t, sizeof(struct Tarefa), cont, arquivo_binario);
-                fclose(arquivo_binario);
-            }
-            printf("Saindo\n");
-            break;
-        } else {
-            printf("Valor invalido!\n\n");
+                break;
+            case 3:
+                printf("Lista:\n\n");
+                for (int x = 0; x < cont; x++) {
+                    printf("Tarefa %d\n", x + 1);
+                    imprimir_tarefa(t[x]);
+                }
+                break;
+            case 4:
+                posicao = menu_alterar();
+                if (posicao >= 1 && posicao <= cont) {
+                    imprimir_tarefa(t[posicao - 1]);
+                    alterar_tarefa(t, cont, posicao);
+                    printf("Tarefa alterada!\n\n");
+                } else {
+                    printf("Posição inválida!\n\n");
+                }
+                break;
+            case 5:
+                arquivo_binario = fopen("tarefas.txt", "wb");
+                if (arquivo_binario) {
+                    fwrite(t, sizeof(struct Tarefa), cont, arquivo_binario);
+                    fclose(arquivo_binario);
+                }
+                printf("Saindo\n");
+                return 0;
+            default:
+                printf("Valor inválido!\n\n");
         }
     }
 
