b45ff49d4438877e35ed153153d4372936a14d9d
jpeterutto <149285505+jpeterutto@users.noreply.github.com>
2023-11-11 19:00:16 -0300

    Alterar Tarefas adicionado

--- projeto.c
+++ projeto.c
@@ -1,7 +1,6 @@
 #include "projeto.h"
 #include <stdio.h>
 
-
 void limpa() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {}
@@ -32,4 +31,80 @@ void excluir_tarefa(struct Tarefa *tarefas, int *cont, int posicao) {
     } else {
         printf("Posição inválida!\n\n", *cont);
     }
+}
+
+void alterar_tarefa(struct Tarefa *tarefas, int cont, int posicao) {
+    int opcao;
+    char *p;
+    char s_opcao[100];
+
+    printf("Escolha o campo que deseja alterar:\n");
+    printf("1 - Prioridade\n");
+    printf("2 - Descrição\n");
+    printf("3 - Categoria\n");
+    printf("4 - Status\n");
+    printf("Digite o número da opção: ");
+    fgets(s_opcao, sizeof(s_opcao), stdin);
+    opcao = strtol(s_opcao, &p, 10);
+
+    switch (opcao) {
+        case 1:
+            printf("Nova Prioridade: ");
+            scanf("%d", &tarefas[posicao - 1].prioridade);
+            limpa();
+            break;
+        case 2:
+            printf("Nova Descrição: ");
+            scanf("%299[^\n]", tarefas[posicao - 1].descricao);
+            limpa();
+            break;
+        case 3:
+            printf("Nova Categoria: ");
+            scanf("%99[^\n]", tarefas[posicao - 1].categoria);
+            limpa();
+            break;
+        case 4:
+            printf("Novo Status (0 - Não Iniciado, 1 - Em Andamento, 2 - Completo): ");
+            scanf("%d", (int*)&tarefas[posicao - 1].status);
+            limpa();
+            break;
+        default:
+            printf("Opção inválida\n");
+    }
+}
+
+int menu_alterar() {
+    char *p;
+    char s_opcao[100];
+    int opcao;
+
+    printf("Digite o número da tarefa que deseja alterar: ");
+    fgets(s_opcao, sizeof(s_opcao), stdin);
+    opcao = strtol(s_opcao, &p, 10);
+
+    return opcao;
+}
+
+void imprimir_tarefa(struct Tarefa tarefa) {
+    printf("Prioridade: %d\n", tarefa.prioridade);
+    printf("Categoria: %s\n", tarefa.categoria);
+    printf("Descricao: %s\n", tarefa.descricao);
+
+    // Mostrar o status da tarefa
+    printf("Status: ");
+    switch (tarefa.status) {
+        case NAO_INICIADO:
+            printf("Não Iniciado\n");
+            break;
+        case EM_ANDAMENTO:
+            printf("Em Andamento\n");
+            break;
+        case COMPLETO:
+            printf("Completo\n");
+            break;
+        default:
+            printf("Desconhecido\n");
+    }
+
+    printf("\n");
 }
