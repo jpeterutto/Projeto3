15aa514eea76360d7920b2ac7f9eea25b7f81e2b
jpeterutto <149285505+jpeterutto@users.noreply.github.com>
2023-11-11 18:35:19 -0300

    Adicionei o Projeto 1

--- projeto.h
+++ projeto.h
@@ -1,1 +1,18 @@
+#ifndef PROJETO_H
+#define PROJETO_H
 
+struct Tarefa {
+    int prioridade;
+    char categoria[100];
+    char descricao[300];
+};
+//Espaço responsável por definir a estrutura da tarefa
+
+void limpa();
+//cabeçalho da função responsável por limpar as informações salvas na variável para poder armazenar novas informações
+void ler(struct Tarefa *armazena);
+//cabeçalho da função que lê as informações dadas pelo usuário e armazena no arquivo
+void excluir_tarefa(struct Tarefa *tarefas, int *cont, int posicao);
+//cabeçalho da função que vai excluir as tarefas
+
+#endif//PROJETO_H
