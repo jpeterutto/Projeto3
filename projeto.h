425e55cd007a5cdf7adca19ab4b6e2005dac98b0
jpeterutto <149285505+jpeterutto@users.noreply.github.com>
2023-11-12 12:44:46 -0300

    Filtrar por Prioridade adicionado

--- projeto.h
+++ projeto.h
@@ -1,6 +1,10 @@
 #ifndef PROJETO_H
 #define PROJETO_H
 
+// João Pedro Lopes - RA: 24.123.071-3
+// João Pedro Peterutto - RA: 24.123.045-7
+// Gabriel Koiama - RA: 24.123.051-5
+
 enum Status {
     NAO_INICIADO,
     EM_ANDAMENTO,
@@ -20,5 +24,6 @@ void excluir_tarefa(struct Tarefa *tarefas, int *cont, int posicao);
 void alterar_tarefa(struct Tarefa *tarefas, int cont, int posicao);
 int menu_alterar();
 void imprimir_tarefa(struct Tarefa tarefa);
+void filtrar_por_prioridade(struct Tarefa *tarefas, int cont, int prioridade);
 
 #endif // PROJETO_H
